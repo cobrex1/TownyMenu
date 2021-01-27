005ec91984fe9f9f87a2a22289225624409988ae
Tolmikarc <benny@bennyc.io>
2021-01-26 19:01:54 -0800

    Reconfigure Git Organization and Fix Kick Prompt not Kicking

--- src/main/java/net/tolmikarc/townymenu/town/prompt/TownKickPrompt.java
+++ src/main/java/net/tolmikarc/townymenu/town/prompt/TownKickPrompt.java
@@ -1,10 +1,6 @@
 package net.tolmikarc.townymenu.town.prompt;
 
-import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.exceptions.EmptyTownException;
-import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.Town;
 import net.tolmikarc.townymenu.settings.Localization;
 import org.bukkit.conversations.ConversationContext;
 import org.bukkit.conversations.Prompt;
@@ -36,16 +32,9 @@ public class TownKickPrompt extends SimplePrompt {
 		if (!getPlayer(context).hasPermission("towny.command.town.kick"))
 			return null;
 
-		try {
-			if (input.toLowerCase().equals(Localization.CONFIRM)) {
-				Town town = resident.getTown();
-				town.removeResident(resident);
-				TownyAPI.getInstance().getDataSource().saveTown(town);
-				TownyAPI.getInstance().getDataSource().saveResident(resident);
-				tell(Localization.TownConversables.Kick.RESPONSE.replace("{player}", resident.getName()));
-			}
-		} catch (EmptyTownException | NotRegisteredException e) {
-			e.printStackTrace();
+		if (input.toLowerCase().equals(Localization.CONFIRM)) {
+			resident.removeTown();
+			tell(Localization.TownConversables.Kick.RESPONSE.replace("{player}", resident.getName()));
 		}
 
 
